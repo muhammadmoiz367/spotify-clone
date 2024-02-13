386e2ed294a3d772bcf666c2d44253fc581cb868
Muhammad Moiz <muhammadmoiz367@gmail.com>
2024-02-14 03:25:09 +0500

    auth modal and supabase setup with layout

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import Sidebar from '@/components/Sidebar';
+import ModalProvider from '@/providers/ModalProvider';
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import ToasterProvider from '@/providers/ToasterProvider';
+import UserProvider from '@/providers/UserProvider';
+import type { Metadata } from 'next';
+import { Figtree } from 'next/font/google';
+import './globals.css';
+
+const font = Figtree({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music!',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider />
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
+}
